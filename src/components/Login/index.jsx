805627bcaa00bc699dda85298d466dbc7d6e4487
swqxdba <983110853@qq.com>
2022-03-02 11:21:39 +0800

    Default Changelist

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import { Form, Input, Button, Checkbox } from 'antd';
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.variable.min.css';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import './index.css'
 
@@ -20,63 +20,65 @@ export class Login extends Component {
          password:''
      }
     render() {
-
-
         return (
 
-            <Form
+            <div className="login-form">
+                <img src={'svg/phone.svg'} alt={''}/>
+                <Form
+                    name="normal_login"
 
-                name="normal_login"
-                className="login-form"
-                initialValues={{ remember: true }}
-                onFinish={this.onFinish}
-                onFinishFailed={this.onFinishFailed}
-            >
-
-                <Form.Item
-                    name="username"
-                    rules = {[{type: 'string',message:'必须为string类型'}
-                        ,{min:6,message: '必须大于6位'}
-                        ,{max:15,message: '最大为15位'}
-                        ,{required: true, message:'必须输入密码'}
-                        ,{pattern: '[A-Za-z0-9_\\-\u4e00-\u9fa5]+', message:'请输入英文或数字或中文字符'}]}
-
+                    initialValues={{ remember: true }}
+                    onFinish={this.onFinish}
+                    onFinishFailed={this.onFinishFailed}
                 >
-                    <Input  prefix={<UserOutlined className="site-form-item-icon" />} placeholder="用户名" />
-                </Form.Item>
-                <Form.Item
-                    name="password"
-                    rules = {[{type: 'string',message:'必须为string类型'}
-                        ,{min:6,message: '必须大于6位'}
-                        ,{max:15,message: '最大为15位'}
-                        ,{required: true, message:'必须输入密码'}
-                        ,{pattern: '[A-Za-z0-9]+', message:'请输入大小写英文或数字'}]
-                    }
 
-                >
-                    <Input
-                        prefix={<LockOutlined className="site-form-item-icon" />}
-                        type="password"
-                        placeholder="密码"
-                    />
-                </Form.Item>
-                <Form.Item>
-                    <Form.Item name="remember" valuePropName="checked" noStyle>
-                        <Checkbox>记住我</Checkbox>
+
+                    <Form.Item
+                        name="username"
+                        rules = {[{type: 'string',message:'必须为string类型'}
+                            ,{min:6,message: '必须大于6位'}
+                            ,{max:15,message: '最大为15位'}
+                            ,{required: true, message:'必须输入密码'}
+                            ,{pattern: '[A-Za-z0-9_\\-\u4e00-\u9fa5]+', message:'请输入英文或数字或中文字符'}]}
+
+                    >
+                        <Input  prefix={<UserOutlined className="site-form-item-icon" />} placeholder="用户名" />
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules = {[{type: 'string',message:'必须为string类型'}
+                            ,{min:6,message: '必须大于6位'}
+                            ,{max:15,message: '最大为15位'}
+                            ,{required: true, message:'必须输入密码'}
+                            ,{pattern: '[A-Za-z0-9]+', message:'请输入大小写英文或数字'}]
+                        }
+
+                    >
+                        <Input
+                            prefix={<LockOutlined className="site-form-item-icon" />}
+                            type="password"
+                            placeholder="密码"
+                        />
+                    </Form.Item>
+                    <Form.Item>
+                        <Form.Item name="remember" valuePropName="checked" noStyle>
+                            <Checkbox>记住我</Checkbox>
+                        </Form.Item>
+
+                        <a className="login-form-forgot" href="">
+                            忘记密码
+                        </a>
                     </Form.Item>
 
-                    <a className="login-form-forgot" href="">
-                        忘记密码
-                    </a>
-                </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" className="login-form-button">
+                            登录
+                        </Button>
+                        Or <a href="./home">注册!</a>
+                    </Form.Item>
+                </Form>
+            </div>
 
-                <Form.Item>
-                    <Button type="primary" htmlType="submit" className="login-form-button">
-                        登录
-                    </Button>
-                    Or <a href="./home">注册!</a>
-                </Form.Item>
-            </Form>
         );
     }
 
